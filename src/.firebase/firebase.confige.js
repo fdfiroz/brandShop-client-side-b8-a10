0652f2a3bdc18e9f52db00e25e203a5a50718efa
fdfiroz <firozahamed0@gmail.com>
2023-10-18 04:26:06 +0600

    Initial setup with react router and firebase

---
+++ src/.firebase/firebase.confige.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM1Lzk2qD_G2fkhf0o97KIUlgZRER1PvY",
+  authDomain: "brand-shop-2a869.firebaseapp.com",
+  projectId: "brand-shop-2a869",
+  storageBucket: "brand-shop-2a869.appspot.com",
+  messagingSenderId: "378187288392",
+  appId: "1:378187288392:web:bd229a9f3f75898364ed7d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
